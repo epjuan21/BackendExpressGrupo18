84378cefbf95ec5dd4d9b8eb1e29fa0ec5c176bf
Juan Ramirez <ep.juan@gmail.com>
2021-10-24 09:40:50 -0500

    Heroku First Push

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const authRoutes = require('./routes/auth.routes')
 
 // Configuraciones
 app.set('port', process.env.PORT || 3000)
+mongoose.connect(process.env.DB_STRING)
+    .then(db => console.log('Connected to Database'))
+    .catch(err => console.error(err))
 
 // Middlewares
 app.use(morgan('dev'))
